2646911a9d255e2fef1c988914b7ef572e903b9d
Iscle <albertiscle9@gmail.com>
2026-09-24 14:49:34 +0200

    airoha: add BE5000 Wi-Fi with per-device calibration in NVMEM
    Hold the stock-required Wi-Fi enable GPIO high and provide its pin mapping. Read calibration from a reserved eraseblock at 0xa0000 instead of embedding device data in the image. Preserve the existing bootloader and firmware offsets. Add metadata-checked sysupgrade support and standard wireless packages without configuring networks or a regulatory country.

--- target/linux/airoha/image/an7563.mk
+++ target/linux/airoha/image/an7563.mk
@@ -23,6 +23,7 @@ define Device/airoha_an7563-evb
   DEVICE_VENDOR := Airoha
   DEVICE_MODEL := AN7563 Evaluation Board
   DEVICE_DTS := an7563-evb
+  DEVICE_PACKAGES += luci uhttpd
   KERNEL_LOADADDR := 0x80088000
   ARTIFACT/preloader.bin := an7563-preloader rfb
   ARTIFACT/bl2-bl31-uboot.bin := an7563-bl2-bl31-uboot rfb
@@ -34,7 +35,8 @@ define Device/xiaomi_be5000
   DEVICE_VENDOR := Xiaomi
   DEVICE_MODEL := BE5000
   DEVICE_DTS := an7563-xiaomi-be5000
-  DEVICE_PACKAGES += kmod-i2c-an7581
+  SUPPORTED_DEVICES += airoha,an7563-evb
+  DEVICE_PACKAGES += luci uhttpd kmod-mt7996e kmod-mt7992-23-firmware wpad-basic-mbedtls iw
   KERNEL_LOADADDR := 0x80088000
   ARTIFACT/preloader.bin := an7563-preloader xiaomi_be5000
   ARTIFACT/bl2-bl31-uboot.bin := an7563-bl2-bl31-uboot xiaomi_be5000
